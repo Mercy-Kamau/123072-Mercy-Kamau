06cb3c1bd1577471c8564097b59c5748cc587c1b
unknown <kamau.mercy@strathmore.edu>
2021-04-26 17:53:19 +0300

    First Commit

---
+++ util.php
@@ -0,0 +1,10 @@
+<?php
+    class Util {
+        
+        static $DB_NAME = "labs";
+        static $DB_USER = "root";
+        static $DB_USER_PASS = "Memusi2020";
+        static $SERVER_NAME = "localhost";
+    }
+    
+?>
